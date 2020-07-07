cb364c94096654e7cbc68f5023d32bbf52b6fec7
Leviosah <conormag60@gmail.com>
2020-07-06 22:25:07 -0700

    first commit

---
+++ questions.js
@@ -0,0 +1,34 @@
+
+var questions = [
+    {
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['strings','booleans','alerts','numbers'],
+        answer: 'alerts'
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed within _______',
+        choices: ['quotes','curly brackets','parentheses','square brackets'],
+        answer: 'parentheses'
+    },
+    {
+        title: 'What is the HTML tag under which one can write the JavaScript code?',
+        choices: ['<javascript>','<scripted>','<script>','<js>'],
+        answer: '<script>'
+    },
+    {
+        title: 'Which of the following is the correct syntax for "coding is fun" in an alert box using JavaScript?',
+        choices: ['alertbox("coding is fun");','msg("coding is fun");','msgbox("coding is fun");','alert("coding is fun");'],
+        answer: 'alert("coding is fun");'
+    },
+    {
+        title: 'What is the proper syntax for creating a function in JavaScript named masterfunc?',
+        choices: ['function = masterfunc()','function masterfunc()','function := masterfunc()','function : masterfunc()'],
+        answer: 'function masterfunc()'
+    },
+    {
+        title: 'What is the method in JavaScript ised to remove the whitespace at the beginning and end of any string?',
+        choices: ['trim()','strip()','cut()','slice()'],
+        answer: 'trim()'
+    }
+];
+
