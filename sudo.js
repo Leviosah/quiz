9d015aa46aa3ce0946f90928dc26bfb9b77789c2
Leviosah <conormag60@gmail.com>
2020-07-08 19:38:03 -0700

    update with build quiz and button event functions

---
+++ sudo.js
@@ -0,0 +1,27 @@
+
+
+// Copding quiz
+
+// JS
+    // 1) Get questions, options & answer
+        // Show the user the question and the options
+
+    // 2) compair
+        // compaire that buttons (options) to the answer
+            // ex: if user selects id:1 compaire id:1 === answer (on our questions array)
+        // If else statement like so {
+
+       // if (userChoice === selectedButton) {
+         //   dont touch Time
+           // move to next question (function) // fill in
+       // } else {
+        //    - 15 seconds time from TimeR
+         //   move to next question (function) // fill in
+        //}
+        //}
+
+    // 3) finishing the game & updating local storage
+        // take the score (all the correct answers) and let the user know their score
+        // update the local storage with the user info
+
+    // BONUS: have user enter in their custom info or whatever 
