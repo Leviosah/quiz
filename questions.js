e604a75b81d932c63ec2d56f9c6e1603a7fabcba
Leviosah <conormag60@gmail.com>
2020-07-10 17:16:31 -0700

    update with timer and endgame function

--- questions.js
+++ questions.js
@@ -1,153 +1,56 @@
-var startEl = document.getElementById("startButton");
-var questionEl = document.getElementById("question");
-var buttonEl = document.querySelector(".button_base b01_simple_rollover");
-var answersEl = document.getElementById("answers");
-var answer0El = document.getElementById("answer0");
-var answer1El = document.getElementById("answer1");
-var answer2El = document.getElementById("answer2");
-var answer3El = document.getElementById("answer3");
-
-
-
 var questions = [
     {
-        title: 'Commonly used data types DO NOT include:',
-        choices: ['strings','booleans','alerts','numbers'],
-        answer: 'alerts'
+        question: 'Commonly used data types DO NOT include:',
+        answers: [
+          { text: 'strings', correct: false},
+          { text: 'booleans', correct: false},
+          { text: 'alerts', correct: true},
+          { text: 'numbers', correct: false},
+        ]
     },
     {
-        title: 'The condition in an if / else statement is enclosed within _______',
-        choices: ['quotes','curly brackets','parentheses','square brackets'],
-        answer: 'parentheses'
+        question: 'The condition in an if / else statement is enclosed within _______',
+        answers: [
+          { text: 'quotes', correct: false},
+          { text: 'curly brackets', correct: false},
+          { text: 'parentheses', correct: true},
+          { text: 'square brackets', correct: false},
+        ]
     },
     {
-        title: 'What is the HTML tag under which one can write the JavaScript code?',
-        choices: ['<javascript>','<scripted>','<script>','<js>'],
-        answer: '<script>'
+        question: 'What is the HTML tag under which one can write the JavaScript code?',
+        answers: [
+          { text: '<javascript>', correct: false},
+          { text: '<scripted>', correct: false},
+          { text: '<js>', correct: false},
+          { text: '<script>', correct: true},
+        ]
     },
     {
-        title: 'Which of the following is the correct syntax for "coding is fun" in an alert box using JavaScript?',
-        choices: ['alertbox("coding is fun");','msg("coding is fun");','msgbox("coding is fun");','alert("coding is fun");'],
-        answer: 'alert("coding is fun");'
+        question: 'Which of the following is the correct syntax for "coding is fun" in an alert box using JavaScript?',
+        answers: [
+          { text: 'alert("coding is fun");', correct: true},
+          { text: 'alertbox("coding is fun");', correct: false},
+          { text: 'msg("coding is fun");', correct: false},
+          { text: 'msgbox("coding is fun");', correct: false},
+        ]
     },
     {
-        title: 'What is the proper syntax for creating a function in JavaScript named masterfunc?',
-        choices: ['function = masterfunc()','function masterfunc()','function := masterfunc()','function : masterfunc()'],
-        answer: 'function masterfunc()'
+        question: 'What is the proper syntax for creating a function in JavaScript named masterfunc?',
+        answers: [
+          { text: 'function = masterfunc()', correct: false},
+          { text: 'function masterfunc()', correct: true},
+          { text: 'function := masterfunc()', correct: false},
+          { text: 'function : masterfunc()', correct: false},
+        ]
     },
     {
-        title: 'What is the method in JavaScript ised to remove the whitespace at the beginning and end of any string?',
-        choices: ['trim()','strip()','cut()','slice()'],
-        answer: 'trim()'
+        question: 'What is the method in JavaScript ised to remove the whitespace at the beginning and end of any string?',
+        answers: [
+          { text: 'trim()', correct: true},
+          { text: 'strip()', correct: false},
+          { text: 'cut()', correct: false},
+          { text: 'slice()', correct: false},
+        ]
     }
 ];
-
-var index = 0;
-var timerInterval;
-var secondsLeft = 90;
-var highScore = [];
-
-function countdownTimer() {
-    var timer = 90;
-    
-    var interval = setInterval(function() {
-        timer--;
-        $('.timer').text(timer);
-
-        if (timer === 0) {
-            clearInterval(timer)
-        }
-        //if question is answered wrong - 15 seconds
-          // compair user choice with the correct answer
-            // if userchoice === questions answer (dont move time)
-            // else if userchoie is not === to quection answer, dec timer down 15 secons
-    }, 1000);
-};
-
-
-beginEl.onclick.
-
-beginEl.addEventListener("click", function (event) {
-    event.preventDefault();
-    document.getElementById("begin").style.visibility = 'hidden';
-    document.getElementById("qblock").style.visibility = 'visible';
-    console.log("I was clicked");
-    setTime();
-    game(event);
-});
-
-function game() {
-    console.log(index)
-    questionEl.textContent = questions[index].question;
-    answer0El.textContent = questions[index].answer[0];
-    answer1El.textContent = questions[index].answer[1];
-    answer2El.textContent = questions[index].answer[2];
-    answer3El.textContent = questions[index].answer[3];
-  };
-
-buttonEl.forEach(function (answerButton) {
-    answerButton.addEventListener("click", function(event) {
-      console.log("check answer clicked")
-      var element = event.target;
-      if (element.textContent !== questions[index].correct) {
-        console.log("wrong")
-        secondsLeft -= 10
-      } else {
-        console.log("correct answer");
-      }
-      if (index < questions.length - 1) {
-        index++
-        game()
-      } else {
-        clearInterval(timerInterval)
-        endGame()
-      }
-    });
-});
-
-function endGame() {
-    document.getElementById("gamescore").textContent = "Score: " + secondsLeft;
-    document.getElementById("qblock").style.visibility = 'hidden';
-    document.getElementById("restart").style.visibility = 'visible';
-    document.getElementById("resethigh").style.visibility = 'visible';
-    document.getElementById("gamescore").style.visibility = 'visible';
-    document.getElementById("time").style.visibility = 'hidden';
-    console.log(secondsLeft)
-    storeHigh()
-  };
-  
-  // restarts game by refreshing page
-  restartEl.addEventListener("click", function (event) {
-    location.reload(event)
-  });
-  
-  // resets high score, leaving html default of "BD : 45"
-  resethighEl.addEventListener("click", function (event) {
-    localStorage.clear(event);
-  });
-  
-  // stores the new/high score
-  function storeHigh() {
-  if ((highScore == 0) || (highScore == null)) {
-    var initials = prompt("What are your initials?");
-    highScore = initials + " " + secondsLeft;
-    localStorage.setItem("highscore", highScore);
-  } else { 
-    if (secondsLeft > highScore) {
-      initials = prompt("What are your initials?");
-      highScore = initials + " " + secondsLeft;
-      localStorage.setItem("highscore", highScore)
-    } else {
-      console.log("not a new high score")
-      alert("Sorry, gotta do a little better next time!") 
-    };
-   };
-  };
-      </script>
-    </body>
-    
-  </html>
-
-
-      
-
